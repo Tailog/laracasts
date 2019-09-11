2e549837264b3c317f7fcf5934b4fbb011703bf7
Santos Pierre <santospierreemmanuel@gmail.com>
2019-09-11 09:48:36 +0200

    Routes And Layouts

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Contact Us</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae beatae aut perspiciatis doloremque quos animi. Dolorem explicabo qui commodi asperiores modi dolores alias eum perspiciatis labore, iure corrupti voluptatem quisquam molestiae cum est. Non iure totam saepe maiores. Minus, eligendi! Consequatur excepturi aliquid temporibus delectus sit eos labore dolor error a placeat tempora deserunt consectetur iusto alias accusamus modi ducimus amet, atque quisquam at illo optio quasi? Perferendis, in! Maiores ut quas sit consectetur deserunt repudiandae quaerat voluptates odit? Consequuntur laudantium sequi quas cum impedit, blanditiis expedita corrupti iure cumque quos explicabo tenetur. Animi saepe rerum maxime aut esse ratione suscipit veniam praesentium excepturi, consectetur perferendis velit accusamus nemo! Illum aliquam dicta earum ea ipsum inventore, ratione magnam. Reiciendis ipsam commodi ratione magnam aliquam harum voluptas itaque architecto, molestiae at cupiditate ut cumque sed facere recusandae repellat qui consequuntur porro quisquam repudiandae omnis accusamus illo. Esse placeat aliquid in odit.</p>
+@endsection
+
+@section('title')
+    Contact Us
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- yields avec un paramètre pour utilisé une valeur par défault --}}
+        <title>@yield('title','Laracast Tuto')</title>
+
+    </head>
+    <body>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+
+      @yield('content')
+        
+    </body>
+</html>
